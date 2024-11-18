453962e1e4cb92794ca3b51f694547a935665f80
Rakibul Hasan Rakib <rhr2001007@gmail.com>
2024-11-18 23:11:06 +0600

    Changed signin, signup button design and implement signin data

--- Frontend/frontend-cloud-server/src/components/SignIn.jsx
+++ Frontend/frontend-cloud-server/src/components/SignIn.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../components/SignIn.module.css'
 const SignIn = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function loginAccount(e){
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password
+    }
+    console.log(userData);
+  }
+
+
   return (
     <div className={styles.mainContainer}>
       <h2>Sign In To Your Account</h2>
-      <form action="" className={styles.inputs}>
-        <input type="email" required placeholder='Email'/>
-        <input type="password" required placeholder='Password'/>
+      <form action="" className={styles.inputs} onSubmit={loginAccount}>
+        <input type="email" required placeholder='Email'onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" required placeholder='Password'onChange={(e) => setPassword(e.target.value)}/>
         <input type="submit" value='Sign In' className={styles.submitButton}/>
         <p>Forgot Password?</p>
       </form>
