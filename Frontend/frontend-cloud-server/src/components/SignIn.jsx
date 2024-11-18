dac3c32e7a2f728e33b2ee55f34821d29f3cd487
Rakibul Hasan Rakib <rhr2001007@gmail.com>
2024-11-18 22:58:32 +0600

    Completed SignIn component

--- Frontend/frontend-cloud-server/src/components/SignIn.jsx
+++ Frontend/frontend-cloud-server/src/components/SignIn.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import styles from '../components/SignIn.module.css'
 const SignIn = () => {
   return (
-    <div className='container'>Signin</div>
+    <div className={styles.mainContainer}>
+      <h2>Sign In To Your Account</h2>
+      <form action="" className={styles.inputs}>
+        <input type="email" required placeholder='Email'/>
+        <input type="password" required placeholder='Password'/>
+        <input type="submit" value='Sign In' className={styles.submitButton}/>
+        <p>Forgot Password?</p>
+      </form>
+    </div>
   );
 }
 
