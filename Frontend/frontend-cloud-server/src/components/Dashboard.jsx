1c2ada8149e80441f0341c7c6dc0f7d0ca15b0b3
Rakibul Hasan Rakib <rhr2001007@gmail.com>
2024-12-01 15:00:17 +0600

    Added basic dashboard headline

--- Frontend/frontend-cloud-server/src/components/Dashboard.jsx
+++ Frontend/frontend-cloud-server/src/components/Dashboard.jsx
@@ -13,6 +13,8 @@ const Dashboard = () => {
     const [token, setToken] = useState(sessionStorage.getItem("cloud-token") || '')
     const navigate = useNavigate()
     const serverURL = URL()
+    const [projects, setProjects] = useState([])
+    let project;
     
     useEffect(() => {
         if(!token){
@@ -28,7 +30,8 @@ const Dashboard = () => {
             .then(response => {
                 if(response.status == 200)
                 {
-                    console.log(response.status);
+                    console.log(typeof(response.data));
+                    loadAllProjects();
 
                 }
             })
@@ -38,19 +41,42 @@ const Dashboard = () => {
             })
         }
     }, [])
+
+
+    function loadAllProjects() {
+        axios.get(`${serverURL}/project/`, {
+            headers: {
+                "Content-Type": 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            if(response.status == 200)
+            {
+                setProjects(response.data)
+                
+            }
+        })
+    }
+
+
+
+
   return (
     <div className={styles.mainContainer}>
 
         <HomePageNavbar />
         <div className={styles.projectContainer}>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quas consequuntur ex cumque nostrum provident nisi beatae temporibus excepturi accusamus alias aliquam error ea quia doloremque officiis, voluptatibus molestiae impedit.</p>
+            <div className={styles.headline}>
+                <p>Your Project List</p>
+                <button>Create Project</button>
+            </div>
+            <table>
+                <th>Title</th>
+                <th>Project Key</th>
+                <th>Number of Sensors</th>
+                <th>Project Creation Date-Time</th>
+            </table>
         </div>
         
 
